7e5a7caf85e15cce7c01e40b5abc28f8e73d0c74
Prahlad Vangeepuram Canchi <80233276+SavageCoder777@users.noreply.github.com>
2024-09-11 19:28:01 -0700

    Upload Recursion Practice
    Upload the file with practice using recursion instead of loops; it includes a forward and backward summation, and a factorial calculator.

---
+++ Practice/recursion.java
@@ -0,0 +1,26 @@
+public class recursion {
+    
+    public int reverseSum(int input) {
+        if (input == 1) {
+            return 1;
+        } else {
+            return (input + reverseSum((input - 1)));
+        }
+    }
+
+    public int forwardSum(int start, int end) {
+        if (start == end) {
+            return end;
+        } else {
+            return (start + forwardSum((start + 1), end));
+        }
+    }
+
+    public int reverseFactorial(int input) {
+        if (input == 1) {
+            return 1;
+        } else {
+            return (input * reverseFactorial((input - 1)));
+        }
+    }
+}
